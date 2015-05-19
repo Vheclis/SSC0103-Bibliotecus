c63b68471fc5237d7c9264968a04f478b8c3faf2
Victor Moneratto <victor.moneratto@gmail.com>
2015-05-18 21:53:48 -0300

    Project start & Main window :books:

---
+++ src/br/usp/icmc/ssc01032015/bibliotecus/Main.java
@@ -0,0 +1,20 @@
+package br.usp.icmc.ssc01032015.bibliotecus;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Stage stage = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
+        stage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
---
+++ src/br/usp/icmc/ssc01032015/bibliotecus/controller/MainController.java
@@ -0,0 +1,51 @@
+package br.usp.icmc.ssc01032015.bibliotecus.controller;
+
+import javafx.event.ActionEvent;
+
+public class MainController
+{
+    public void userSignUp(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void userSignIn(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void bookRegister(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void booksImport(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void usersImport(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void loansImport(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void booksExport(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void usersExport(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void loansExport(ActionEvent actionEvent)
+    {
+
+    }
+}
