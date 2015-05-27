2b0d53d95dcf0d2877aee8166e65958274f6bd31
Victor H. Heclis <vheeclis@gmail.com>
2015-05-27 15:33:57 -0300

    My books and List Users tables

--- src/br/usp/icmc/ssc01032015/bibliotecus/controller/ListUsersTabController.java
+++ src/br/usp/icmc/ssc01032015/bibliotecus/controller/ListUsersTabController.java
@@ -1,5 +1,42 @@
 package br.usp.icmc.ssc01032015.bibliotecus.controller;
 
-public class ListUsersTabController
+import br.usp.icmc.ssc01032015.bibliotecus.model.Library;
+import br.usp.icmc.ssc01032015.bibliotecus.model.User;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ListUsersTabController implements Initializable
 {
+    @FXML
+    private TableView usersTable;
+
+    @FXML
+    private TableColumn nameCol;
+
+    @FXML
+    private TableColumn typeCol;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+        Library.getInstance().getUsers().add(new User("Marcus", User.Type.Community));
+        Library.getInstance().getUsers().add(new User("Mello", User.Type.Professor));
+        Library.getInstance().getUsers().add(new User("Victor", User.Type.Student));
+
+        usersTable.setItems(Library.getInstance().getUsers());
+
+        nameCol.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<User, String>("typeName"));
+
+    }
 }
--- src/br/usp/icmc/ssc01032015/bibliotecus/controller/MyBooksTab.java
+++ src/br/usp/icmc/ssc01032015/bibliotecus/controller/MyBooksTab.java
@@ -1,6 +1,37 @@
 package br.usp.icmc.ssc01032015.bibliotecus.controller;
 
 
-public class MyBooksTab
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyBooksTab implements Initializable
 {
+    @FXML
+    private TableView myBooksTable;
+
+    @FXML
+    private TableColumn bookCol;
+
+    @FXML
+    private TableColumn checkOutCol;
+
+    @FXML
+    private TableColumn dueDateCol;
+
+    @FXML
+    private TableColumn statusCol;
+
+    @FXML
+    private TableColumn suspensionCol;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
 }
